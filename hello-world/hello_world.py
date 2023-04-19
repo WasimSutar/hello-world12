63ab95604690dc1e69ededd2f601b5a42662215c
WasimSutar <waseemsutar@gmail.com>
2023-04-19 15:07:09 +0530

    review comment

--- hello-world/hello_world.py
+++ hello-world/hello_world.py
@@ -6,6 +6,6 @@ def print_name(myname : str = ''):
 ##############
 if __name__ == '__main__':
     print('Hello world')
-    print('Print my name:')
+    print('Print my name:::::')
     myname = 'Chethan'
     print_name(f'My name is : {myname}')
